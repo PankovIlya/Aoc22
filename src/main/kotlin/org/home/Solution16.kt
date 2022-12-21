53a0761e577af48b79ec2937cbf6d8027429ce71
ipankov <ipankov@mplat.io>
2022-12-21 19:33:45 +0300

    AOC 16 optimize

--- src/main/kotlin/org/home/Solution16.kt
+++ src/main/kotlin/org/home/Solution16.kt
@@ -11,23 +11,22 @@ fun solution16() {
 
     println("Solution 16:")
     println("   test ${part1(inputListTest) == 1651 && part2(inputListTest) == 1707}")
+    println("   part 1 answer ${part1(inputList)}") // 2114
     var time = now().toEpochMilli()
-    println("   part 1 answer ${part1(inputList)} time = ${now().toEpochMilli() - time}") // 2114
-    time = now().toEpochMilli()
     println("   part 2 answer ${part2(inputList)} time = ${now().toEpochMilli() - time} ms") //2666
 }
 
 
 class Dijkstra(
-    private val main: Point,
-    private val edges: Map<Point, List<Point>>
+    private val main: Volkan.Point,
+    private val edges: Map<Volkan.Point, List<Volkan.Point>>
 ) {
-    fun calc(): MutableMap<Point, Int> {
+    fun calc(): MutableMap<Volkan.Point, Int> {
 
-        val marked = mutableSetOf<Point>()
-        val points = mutableMapOf<Point, Int>()
+        val marked = mutableSetOf<Volkan.Point>()
+        val points = mutableMapOf<Volkan.Point, Int>()
 
-        val compareByLength: Comparator<Point> = compareBy { points[it] ?: Int.MAX_VALUE }
+        val compareByLength: Comparator<Volkan.Point> = compareBy { points[it] ?: Int.MAX_VALUE }
         val ways = PriorityQueue(compareByLength)
         ways.add(main)
         points[main] = 0
@@ -45,74 +44,63 @@ class Dijkstra(
         return points
     }
 
-    data class Point(
-        val name: String,
-        val rate: Int,
-    )
 }
 
-class Volkan {
-
+class Volkan(
+    private val distance: Map<Pair<Point, Point>, Int>
+) {
 
     private val cache2 = mutableMapOf<Key, Int>()
 
     fun part1(
         n: Int,
-        current: Dijkstra.Point,
-        ways: List<Dijkstra.Point>,
-        distance: Map<Pair<Dijkstra.Point, Dijkstra.Point>, Int>
+        current: Point,
+        ways: List<Point>,
     ): Int {
         if (n <= 0) return 0
+
         if (ways.isEmpty()) return current.rate * n
 
         val cacheVal = Key(n, current, ways.toSet())
-        val max = cache2[cacheVal]
 
-        if (max != null){
-            return max + current.rate * n
-        }
-
-
-        val result = ways.map { next ->
+        val max = cache2[cacheVal] ?: ways.map { next ->
             part1(
                 n - distance[Pair(current, next)]!! - 1,
                 next,
-                ways.toList() - next,
-                distance
+                ways.toList() - next
             )
-        }.maxOf { it }
+        }.maxOf { it } + current.rate * n
 
-        cache2[cacheVal] = result
+        cache2[cacheVal] = max
 
-        return result + current.rate * n
-        //cache[Pair<1, set {2,3}>] = max + cost(1) * n
+        return max
     }
 
 
-    data class Key(val i: Int, val current: Dijkstra.Point, val toSet: Set<Dijkstra.Point>)
-
+    data class Key(val i: Int, val current: Volkan.Point, val toSet: Set<Point>)
 
     fun part2(
-        first: Dijkstra.Point,
-        combinations: List<List<Dijkstra.Point>>,
-        ratePoints: Set<Dijkstra.Point>,
-        way: Map<Pair<Dijkstra.Point, Dijkstra.Point>, Int>
+        n: Int,
+        first: Point,
+        ratePoints: Set<Point>
     ): Int {
-        println(combinations.size)
-        println()
-        var i = 0
-        return combinations.map {
-            print("\r")
-            print(i)
-            i += 1
-
-            val set1 = it//.toSet()
-            val set2 = ratePoints - set1
-
-            part1(26, first, set1, way) + part1(26, first,  set2.toList(), way)
-
-        }.maxOf { it }
+        val combinations = combinationsAll(ratePoints.toList())
+        var i = -1
+        val result = combinations
+            .map {
+                ++i
+                //print("\r")
+                //print("   combination ${(i.toDouble()/2 * 100).div(combinations.size.div(2)).toInt()}%")
+                part1(n, first, it.toList()) + part1(n, first, (ratePoints - it).toList())
+            }.maxOf { it }
+        //println()
+        return result
     }
+
+    data class Point(
+        val name: String,
+        val rate: Int,
+    )
 
 }
 
@@ -122,7 +110,7 @@ fun <T> combinations(lst: List<T>, k: Int): List<List<T>> {
     }
     val local = mutableListOf<List<T>>()
 
-    for (idx in 0.rangeTo(lst.size - 1 )) {
+    for (idx in 0.rangeTo(lst.size - 1)) {
         for (item in combinations(lst.subList(idx + 1, lst.size), k - 1)) {
             local.add(mutableListOf(lst[idx]) + item)
         }
@@ -133,7 +121,7 @@ fun <T> combinations(lst: List<T>, k: Int): List<List<T>> {
 
 fun <T> combinationsAll(lst: List<T>): List<List<T>> {
     val local = mutableListOf<List<T>>()
-    for (k in 1.rangeTo(lst.size)){
+    for (k in 1.rangeTo(lst.size)) {
         local.addAll(combinations(lst, k))
     }
     return local
@@ -142,22 +130,26 @@ fun <T> combinationsAll(lst: List<T>): List<List<T>> {
 private fun part1(inputList: List<String>): Int {
     val points = parseInputPoint(inputList)
     val edges = parseInputEdges(inputList, points.associateBy { it.name })
+
+    val ratePoints = points.filter { it.name != "AA" }.filter { it.rate > 0 }.toList()
     val way = points.flatMap { Dijkstra(it, edges).calc().map { kv -> Pair(it, kv.key) to kv.value } }.toMap()
-    return Volkan().part1(30,
-        points.first { it.name == "AA" }, points.filter { it.name != "AA" }.filter { it.rate > 0 }, way
+
+    return Volkan(way).part1(
+        30, points.first { it.name == "AA" }, ratePoints,
     )
 }
 
 private fun part2(inputList: List<String>): Int {
     val points = parseInputPoint(inputList)
     val edges = parseInputEdges(inputList, points.associateBy { it.name })
+
     val way = points.flatMap { Dijkstra(it, edges).calc().map { kv -> Pair(it, kv.key) to kv.value } }.toMap()
     val ratePoints = points.filter { it.name != "AA" }.filter { it.rate > 0 }.toList()
-    val combinations = combinationsAll( ratePoints)
-    return Volkan().part2( points.first { it.name == "AA" }, combinations, ratePoints.toSet(), way)
+
+    return Volkan(way).part2(26, points.first { it.name == "AA" }, ratePoints.toSet())
 }
 
-fun parseInputEdges(inputList: List<String>, points: Map<String, Dijkstra.Point>) =
+fun parseInputEdges(inputList: List<String>, points: Map<String, Volkan.Point>) =
     inputList.associate {
         val arr = it.split("; ")
 
@@ -171,7 +163,7 @@ fun parseInputEdges(inputList: List<String>, points: Map<String, Dijkstra.Point>
 fun parseInputPoint(inputList: List<String>) =
     inputList.map {
         val arr = it.split("; ")[0].split(" ")
-        Dijkstra.Point(
+        Volkan.Point(
             arr[1],
             arr[4].split("=")[1].toInt()
         )
